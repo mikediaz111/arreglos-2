a1b974fb4669599fee50cbe0843ca42a84849b7e
Miguel <miguel.diaz5@catolica.edu.sv>
2021-03-04 23:22:33 -0600

    segundo commit

---
+++ Module1.vb
@@ -0,0 +1,19 @@
+﻿Module module1
+
+    Sub main()
+        Dim numeros(9) As Integer
+
+        Dim suma As Integer = 0
+
+        For i = 0 To numeros.Length - 1 Step 1
+
+            Console.WriteLine("ingrese in valor: " & i + 1)
+            numeros(i) = Console.ReadLine()
+
+        Next
+
+        Console.WriteLine("suma total:  " & suma)
+        Console.ReadLine()
+
+    End Sub
+End Module
